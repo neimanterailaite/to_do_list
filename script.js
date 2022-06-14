6bf25e8a21094dae4b6f973ddee269d04e6c9c0a
Fennec92 <nazar.hamidulla@posteo.de>
2022-06-14 13:55:09 +0200

    initial files

---
+++ script.js
@@ -0,0 +1,20 @@
+const todoBody = document.querySelector(".todo-body");
+const addTask = document.querySelector("#addTask");
+
+const newTask = `
+<div class="task">
+    <div class="task-head">
+        <div class="task-name"></div>
+    </div>
+    <div class="task-body">
+        <button id="task-start">start</button>
+        <button id="task-change">change</button>
+        <button id="task-details">details</button>
+    </div>
+</div>`;
+
+addTask.addEventListener("click", ()=> {
+
+    todoBody.innerHTML+=newTask;
+    todoBody.insertAdjacentElement("afterbegin", "p")
+});
