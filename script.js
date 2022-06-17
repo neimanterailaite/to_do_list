c59b1242ee600c937bf5fe1348b5c8c786548826
Neimante <neimante.railaite@gmail.com>
2022-06-17 16:45:50 +0200

    version 1706

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
 const todoBody = document.querySelector(".todo-body");
-const addTask = document.querySelector("#addTask");
+const progressBody = document.querySelector(".progress-body");
+const doneBody = document.querySelector(".done-body");
+const addTaskButton = document.querySelector("#addTask");
+const inputTaskName= document.querySelector("#newTaskName")
 
-const newTask = `
-<div class="task">
-    <div class="task-head">
-        <div class="task-name"></div>
-    </div>
-    <div class="task-body">
-        <button id="task-start">start</button>
-        <button id="task-delete">delete</button>
-        <button id="task-details">details</button>
-    </div>
-</div>`;
+addTaskButton.addEventListener("click", createNewTask);
 
-addTask.addEventListener("click", ()=> {
+function createNewTask(){
 
-    todoBody.innerHTML+=newTask;
-    // todoBody.insertAdjacentElement("afterbegin", "p")
-    const deleteTask = document.querySelectorAll("#task-delete");
-    
-    deleteTask.forEach(e => {
-        e.addEventListener("click", (e)=>{
-            e.target.closest(".task").remove();
-        })
-    })
+    if(inputTaskName.value){
+        const newTask = document.createElement("div");
+        newTask.classList.add("task");
 
-})
+        const newTaskHead = document.createElement("div");
+        newTaskHead.classList.add("task-head");
+        newTaskHead.textContent=inputTaskName.value;
+        inputTaskName.value="";
+
+        const newTaskBody = document.createElement("div");
+        newTaskBody.classList.add("task-body")
+
+        const startButton = document.createElement("button");
+        const deleteButton = document.createElement("button");
+
+        startButton.textContent="start";
+        deleteButton.textContent="delete";
+
+        startButton.addEventListener("click", startTask);
+        deleteButton.addEventListener("click", deleteTask);
+
+        newTaskBody.append(startButton,deleteButton);
+
+        newTask.append(newTaskHead, newTaskBody);
+
+        return todoBody.append(newTask);
+    }
+
+    return alert("You need to input a task name!")
+}
+
+function startTask(e) {
+    const doneButton = document.createElement("button");
+    doneButton.textContent="done";
+    doneButton.addEventListener("click", doneTask);
+
+    const inProgressTask = e.target;
+    progressBody.append(inProgressTask.closest(".task"));
+    return inProgressTask.closest(".task-body").replaceChild(doneButton, inProgressTask);
+}
+
+function doneTask(e) {
+    const doneTask = e.target;
+    doneBody.append(doneTask.closest(".task"));
+    return doneTask.remove();
+}
+
+function deleteTask(e) {
+    const deleteTask = e.target;
+    return deleteTask.closest(".task").remove();
+}
 
 
-console.log(newTask)
-const startbutton = document.querySelector("#task-start");
 
-const starttask = startbutton.addEventListener("click", () => {
-    document.querySelector('.progress-body').appendChild(document.querySelector(newTask))
-
-})
