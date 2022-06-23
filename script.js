2f4c65ead7615e5613586d40bbcda0ed3c57d05b
Fennec92 <nazar.hamidulla@posteo.de>
2022-06-23 14:09:33 +0200

    dont know what the hell i am doing

--- script.js
+++ script.js
@@ -5,85 +5,92 @@ const doneBody = document.querySelector(".done-body");
 const addTaskButton = document.querySelector("#addTask");
 const inputTaskName = document.querySelector("#newTaskName");
 
+const todoStorageArray = JSON.parse(localStorage.getItem("todo")) || [];
+const progressStorageArray = JSON.parse(localStorage.getItem("progress")) || [];
+const doneStorageArray = JSON.parse(localStorage.getItem("done")) || [];
+
 class Task {
-  constructor(status = 0) {
-    this._name = inputTaskName;
+  constructor(status=0) {
+    this._name = inputTaskName.value;
     this._status = status;
   }
-}
+
+};
 
 addTaskButton.addEventListener("click", () => {
   const newTask = new Task();
+  todoStorageArray.push(newTask);
+  localStorage.setItem("todo", JSON.stringify(todoStorageArray));
 });
 
-function createNewTask() {
-  if (inputTaskName.value) {
-    const newTask = document.createElement("div");
-    newTask.classList.add("task");
+// function createNewTask() {
+//   if (inputTaskName.value) {
+//     const newTask = document.createElement("div");
+//     newTask.classList.add("task");
 
-    const newTaskHead = document.createElement("div");
-    newTaskHead.classList.add("task-head");
+//     const newTaskHead = document.createElement("div");
+//     newTaskHead.classList.add("task-head");
 
-    const newTaskInputField = document.createElement("textarea");
-    newTaskInputField.value = inputTaskName.value;
-    newTaskInputField.classList.add("input");
-    newTaskInputField.disabled = true;
-    inputTaskName.value = "";
+//     const newTaskInputField = document.createElement("textarea");
+//     newTaskInputField.value = inputTaskName.value;
+//     newTaskInputField.classList.add("input");
+//     newTaskInputField.disabled = true;
+//     inputTaskName.value = "";
 
-    const newTaskBody = document.createElement("div");
-    newTaskBody.classList.add("task-body");
+//     const newTaskBody = document.createElement("div");
+//     newTaskBody.classList.add("task-body");
 
-    const startButton = document.createElement("button");
-    const deleteButton = document.createElement("button");
-    const editButton = document.createElement("button");
+//     const startButton = document.createElement("button");
+//     const deleteButton = document.createElement("button");
+//     const editButton = document.createElement("button");
 
-    startButton.textContent = "start";
-    deleteButton.textContent = "delete";
-    editButton.textContent = "edit";
+//     startButton.textContent = "start";
+//     deleteButton.textContent = "delete";
+//     editButton.textContent = "edit";
 
-    startButton.addEventListener("click", startTask);
-    deleteButton.addEventListener("click", deleteTask);
-    editButton.addEventListener("click", editTask);
+//     startButton.addEventListener("click", startTask);
+//     deleteButton.addEventListener("click", deleteTask);
+//     editButton.addEventListener("click", editTask);
 
-    newTaskHead.append(newTaskInputField);
+//     newTaskHead.append(newTaskInputField);
 
-    newTaskBody.append(startButton, deleteButton, editButton);
+//     newTaskBody.append(startButton, deleteButton, editButton);
 
-    newTask.append(newTaskHead, newTaskBody);
+//     newTask.append(newTaskHead, newTaskBody);
 
-    todoBody.append(newTask);
+//     todoBody.append(newTask);
 
-    return;
-  }
+//     return;
+//   }
 
-  return alert("You need to input a task name!");
-}
+//   return alert("You need to input a task name!");
+// }
 
-function startTask(e) {
-  const doneButton = document.createElement("button");
-  doneButton.textContent = "done";
-  doneButton.addEventListener("click", doneTask);
+// function startTask(e) {
+//   const doneButton = document.createElement("button");
+//   doneButton.textContent = "done";
+//   doneButton.addEventListener("click", doneTask);
 
-  const inProgressTask = e.target;
-  progressBody.append(inProgressTask.closest(".task"));
-  return inProgressTask
-    .closest(".task-body")
-    .replaceChild(doneButton, inProgressTask);
-}
+//   const inProgressTask = e.target;
+//   progressBody.append(inProgressTask.closest(".task"));
+//   return inProgressTask
+//     .closest(".task-body")
+//     .replaceChild(doneButton, inProgressTask);
+// }
 
-function editTask(e) {
-  const editTask = e.target.closest(".task");
-  editTask.firstChild.firstChild.disabled =
-    !editTask.firstChild.firstChild.disabled;
-}
+// function editTask(e) {
+//   const editTask = e.target.closest(".task");
+//   editTask.firstChild.firstChild.disabled =
+//     !editTask.firstChild.firstChild.disabled;
+// }
 
-function doneTask(e) {
-  const doneTask = e.target;
-  doneBody.append(doneTask.closest(".task"));
-  return doneTask.remove();
-}
+// function doneTask(e) {
+//   const doneTask = e.target;
+//   doneBody.append(doneTask.closest(".task"));
+//   return doneTask.remove();
+// }
 
-function deleteTask(e) {
-  const deleteTask = e.target;
-  return deleteTask.closest(".task").remove();
-}
+// function deleteTask(e) {
+//   const deleteTask = e.target;
+//   return deleteTask.closest(".task").remove();
+// }
